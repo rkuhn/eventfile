f14cc989543a31f96e7e0f52e183b9bbf5fde110
Roland Kuhn <rk@rkuhn.info>
2022-06-03 17:58:10 +0200

    add EventFile

---
+++ src/bin/example.rs
@@ -0,0 +1,12 @@
+use eventfile::{Error, EventFile, EventFrame};
+
+fn main() -> Result<(), Error> {
+    let mut f = EventFile::open("file")?;
+    f.append(EventFrame::new(b"12345", b"abcd"))?;
+    f.append(EventFrame::new(b"a12345", b"abcd567"))?;
+    f.append(EventFrame::new(b".12345", b"abcddfghd"))?;
+    for ev in &mut f.iter()? {
+        println!("sig: {} data: {}", ev.signature.len(), ev.data.len());
+    }
+    Ok(())
+}
