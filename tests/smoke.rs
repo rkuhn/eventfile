8da3d4d5bc7175c484dee7bc8317fba74e1c3776
Roland Kuhn <rk@rkuhn.info>
2022-07-28 18:58:26 +0200

    add per_byte smoke test

--- tests/smoke.rs
+++ tests/smoke.rs
@@ -4,6 +4,7 @@ use rand::{thread_rng, Rng};
 use smallvec::SmallVec;
 use std::{
     borrow::Cow,
+    collections::BTreeMap,
     ops::{Bound, RangeBounds, RangeInclusive},
 };
 use tempfile::{tempdir, TempDir};
@@ -39,6 +40,42 @@ where
             }
         }
         Bound::Unbounded => Bound::Included(right.end().as_ref()),
+    };
+    match (min, max) {
+        (Bound::Included(l), Bound::Included(r)) => l <= r,
+        (Bound::Included(l), Bound::Excluded(r)) => l < r,
+        (Bound::Excluded(l), Bound::Included(r)) => l < r,
+        (Bound::Excluded(l), Bound::Excluded(r)) => l < r,
+        _ => unreachable!(),
+    }
+}
+
+fn overlaps2<T, L>(left: &L, right: RangeInclusive<T>) -> bool
+where
+    T: Ord,
+    L: RangeBounds<T>,
+{
+    let min = match left.start_bound() {
+        std::ops::Bound::Included(left) => Bound::Included(left.max(right.start())),
+        std::ops::Bound::Excluded(left) => {
+            if left >= right.start() {
+                Bound::Excluded(left)
+            } else {
+                Bound::Included(right.start())
+            }
+        }
+        std::ops::Bound::Unbounded => Bound::Included(right.start()),
+    };
+    let max = match left.end_bound() {
+        Bound::Included(left) => Bound::Included(left.min(right.end())),
+        Bound::Excluded(left) => {
+            if left <= right.end() {
+                Bound::Excluded(left)
+            } else {
+                Bound::Included(right.end())
+            }
+        }
+        Bound::Unbounded => Bound::Included(right.end()),
     };
     match (min, max) {
         (Bound::Included(l), Bound::Included(r)) => l <= r,
@@ -200,6 +237,46 @@ fn select<'a>(
     }
 }
 
+fn select_byte(
+    range: impl RangeBounds<u8>,
+) -> impl FnMut(NodeType, &[u8]) -> Result<SmallVec<[u32; FANOUT]>, Error> {
+    move |nt, idx| match nt {
+        NodeType::Branch => Ok(Cbor::checked(idx)
+            .unwrap()
+            .decode()
+            .as_array()
+            .unwrap()
+            .iter()
+            .enumerate()
+            .filter_map(|(idx, cbor)| {
+                let min = get_u64(cbor, "min_fst") as u8;
+                let max = get_u64(cbor, "max_fst") as u8;
+                if overlaps2(&range, min..=max) {
+                    Some(idx as u32)
+                } else {
+                    None
+                }
+            })
+            .collect()),
+        NodeType::Leaf => Ok(Cbor::checked(idx)
+            .unwrap()
+            .decode()
+            .as_array()
+            .unwrap()
+            .iter()
+            .enumerate()
+            .filter_map(|(idx, cbor)| {
+                let fst = get_u64(cbor, "first") as u8;
+                if range.contains(&fst) {
+                    Some(idx as u32)
+                } else {
+                    None
+                }
+            })
+            .collect()),
+    }
+}
+
 fn extractor(bytes: &[u8]) -> Result<String, Error> {
     Ok(get_str(Cbor::checked(bytes).unwrap(), "[0]").into_owned())
 }
@@ -213,11 +290,13 @@ fn smoke() {
     let mut s = Stream::new(13, config, dir.path().join("1234"), 42).unwrap();
 
     let mut all = vec![];
+    let mut per_byte = BTreeMap::<u8, Vec<String>>::new();
     for i in 0..130u64 {
         let cbor = CborBuilder::new().encode_array(|b| {
             b.encode_str(i.to_string());
             let mut bytes = vec![0u8; thread_rng().gen_range(100..300)];
             thread_rng().fill(&mut *bytes);
+            per_byte.entry(bytes[0]).or_default().push(i.to_string());
             b.encode_bytes(bytes);
         });
         s.append(EventFrame::new(cbor.as_slice(), &[])).unwrap();
@@ -245,6 +324,20 @@ fn smoke() {
                 .filter(|s| s.starts_with(lower.as_str()))
                 .cloned()
                 .collect::<Vec<_>>()
+        );
+    }
+
+    for b in 0..=255u8 {
+        let evs = s
+            .iter(select_byte(b..=b), extractor)
+            .unwrap()
+            .map(|r| r.unwrap())
+            .collect::<Vec<_>>();
+        assert_eq!(
+            evs,
+            per_byte.get(&b).cloned().unwrap_or_default(),
+            "b={}",
+            b
         );
     }
 }
